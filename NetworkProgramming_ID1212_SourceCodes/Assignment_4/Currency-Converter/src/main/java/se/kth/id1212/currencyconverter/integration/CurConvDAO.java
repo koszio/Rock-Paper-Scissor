df00b9fdd963a2825d6a3f11eb837cc5196c1b09
koszio <koszio@gmail.com>
2017-12-12 19:41:22 +0100

    Final-changes in design

---
+++ NetworkProgramming_ID1212_SourceCodes/Assignment_4/Currency-Converter/src/main/java/se/kth/id1212/currencyconverter/integration/CurConvDAO.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package se.kth.id1212.currencyconverter.integration;
+
+import javax.ejb.Stateless;
+import javax.ejb.TransactionAttribute;
+import javax.ejb.TransactionAttributeType;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityNotFoundException;
+import javax.persistence.PersistenceContext;
+import se.kth.id1212.currencyconverter.model.Currency;
+
+/**
+ *
+ * @author koszio
+ */
+
+@TransactionAttribute(TransactionAttributeType.MANDATORY)
+@Stateless
+public class CurConvDAO {
+    
+    
+     @PersistenceContext(unitName = "Currency-ConverterPU")
+    private EntityManager em;
+    
+    
+     public Currency converter(String fromCurr, String toCurr, Double amount){
+         
+        Currency currency = em.find(Currency.class, fromCurr);
+        if (currency == null) {
+            throw new EntityNotFoundException("Currency does not exist " + fromCurr);
+        }
+        
+        return currency;
+        
+    }
+    
+}
